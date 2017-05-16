2af0de1196a5fed983bb6c1b097ab96a9f8f83fa
lyvggg <243488627@qq.com>
2017-05-16 10:43:56 +0800

    第二次上传

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
-/**
- * Created by LY on 2017/5/15.
- */
-var gulp = require("gulp");
-gulp.task("show",function () {
-    console.log("hello gulp");
-})
+var gulp=require("gulp");
+var less=require("less");
+var $=require('gulp-load-plugins')();
+// var cleancss=require("gulp-clean-css");
+// var concat=require("gulp-concat");
+//less文件编译成css文件并压缩
+gulp.task('less',function () {
+    return gulp.src('app/styles/*.less')
+        .pipe($.less())
+        // .pipe(cleancss())//压缩
+        .pipe(gulp.dest('dist/css'));
+});
+/*当改变代码的时候改变*/
+gulp.task("dist",function () {
+    gulp.watch("app/styles/*.less",['less']);//编译和压缩CSS文件
+});
+
