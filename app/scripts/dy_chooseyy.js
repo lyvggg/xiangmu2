cfc17b52fe58fc2709c69c82d71c80c679b58499
DeceitM <275718871@qq.com>
2017-05-19 12:34:46 +0800

    JS代码增加

--- app/scripts/dy_chooseyy.js
+++ app/scripts/dy_chooseyy.js
@@ -4,10 +4,12 @@
 $(function () {
     $("#dropdown1").click(function () {
         $("#dropdownlist1").toggle();
+        $("#dropdownlist2").hide();
     })
 });
 $(function () {
     $("#showlist2").click(function () {
         $("#dropdownlist2").toggle();
+        $("#dropdownlist1").hide();
     })
 });
