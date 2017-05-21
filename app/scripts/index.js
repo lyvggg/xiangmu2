cc40995a32e1dc8293f54d1e4313d82d98a7cde9
lyvggg <243488627@qq.com>
2017-05-21 21:52:17 +0800

    超链接已修改完

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,8 @@
 /**
  * Created by LY on 2017/5/15.
  */
-function caidan() {
-    var dropdown_content=document.getElementById("dropdown_content");
-    dropdown_content.style.display="inline-block";
-}
+$(function () {
+    $("#dropdown").click(function () {
+        $("#dropdown_content").toggle();
+    })
+});
